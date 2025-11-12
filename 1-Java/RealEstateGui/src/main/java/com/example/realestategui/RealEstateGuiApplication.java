1c761283a6feeabe6d57783a362bcdf000773f3a
Benkolon152 <v.benasz@gmail.com>
2025-11-12 12:33:07 +0100

    Create Layout

--- 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiApplication.java
+++ 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiApplication.java
@@ -11,7 +11,7 @@ public class RealEstateGuiApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RealEstateGuiApplication.class.getResource("RealEstateGui-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Ingatlanok");
         stage.setScene(scene);
         stage.show();
