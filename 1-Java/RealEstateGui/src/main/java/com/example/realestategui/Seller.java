d02ee1a13afaaa303c22710212c04291326b4400
Benkolon152 <v.benasz@gmail.com>
2025-11-12 12:38:39 +0100

    Copy classes from CLI

---
+++ 1-Java/RealEstateGui/src/main/java/com/example/realestategui/Seller.java
@@ -0,0 +1,38 @@
+package com.example.realestategui;
+
+public class Seller {
+    public int id;
+    public String name;
+    public String phone;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Seller(int id, String name) {
+            setId(id);
+            setName(name);
+        }
+    public Seller(int id, String name, String phone) {
+        setId(id);
+        setName(name);
+        setPhone(phone);
+    }
+
+}
