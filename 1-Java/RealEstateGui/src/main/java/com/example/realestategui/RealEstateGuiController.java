3c2a5eecfad5bcdae2ff74268d6a877497b0a907
Benkolon152 <v.benasz@gmail.com>
2025-11-12 11:58:06 +0100

    Refactor files

--- 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
+++ 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
@@ -3,7 +3,7 @@ package com.example.realestategui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RealEstateGuiController {
     @FXML
     private Label welcomeText;
 
