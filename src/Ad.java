e327961fbb78bc028af62f4efb8ed22093118758
Benkolon152 <v.benasz@gmail.com>
2025-11-12 08:32:21 +0100

    Create Ad class

---
+++ src/Ad.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class Ad {
+    public int area;
+    public Category category;
+    public LocalDate createAt;
+    public String description;
+    public int floors;
+    public boolean freeOfCharge;
+    public int id;
+    public String imageUrl;
+    public String latLong;
+    public int rooms;
+    public Seller seller;
+
+}
