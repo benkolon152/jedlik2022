c53900af0af8c8a25c6eee8edecb83d0ae78f745
Benkolon152 <v.benasz@gmail.com>
2025-11-12 08:40:37 +0100

    Implement LoadFromCsv

--- src/Ad.java
+++ src/Ad.java
@@ -1,4 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ad {
     public int area;
@@ -13,4 +18,19 @@ public class Ad {
     public int rooms;
     public Seller seller;
 
+    public static List<Ad> LoadFromCsv(String filename) throws IOException {
+        List<Ad> ads = new ArrayList<>();
+        StringBuilder csvText = new StringBuilder();
+        FileReader f = new FileReader(filename);
+        int c;
+        while ((c=f.read()) > -1) csvText.append((char)c);
+        f.close();
+
+        String[] lines = csvText.toString().split("\n");
+        for (String line: lines){
+            ads.add(new Ad());
+        }
+
+        return ads;
+    }
 }
