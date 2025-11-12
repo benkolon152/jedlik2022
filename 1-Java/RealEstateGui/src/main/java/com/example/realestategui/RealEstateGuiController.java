6657d8627fa499e70767a7ac9baeca77a68a82ee
Benkolon152 <v.benasz@gmail.com>
2025-11-12 12:57:14 +0100

    Show names on ListView

--- 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
+++ 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
@@ -1,14 +1,16 @@
 package com.example.realestategui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class RealEstateGuiController implements Initializable{
@@ -29,6 +31,17 @@ public class RealEstateGuiController implements Initializable{
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ingatlanok", "root", "");
+
+            Statement stmtNames = conn.createStatement();
+            ResultSet result = stmtNames.executeQuery("SELECT id, name, phone FROM sellers ORDER BY name ASC;");
+            List<String> names = new ArrayList<>();
+            while (result.next()){
+                names.add(result.getString("name"));
+            }
+
+            ObservableList<String> namesOList = FXCollections.observableArrayList(names);
+            listView.setItems(namesOList);
+
             debugger();
         } catch (SQLException e) {
             e.printStackTrace();
