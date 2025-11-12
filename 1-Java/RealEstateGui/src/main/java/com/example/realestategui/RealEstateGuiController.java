86be230c993110ed9c0e6a24ae4f6c210ff0c239
Benkolon152 <v.benasz@gmail.com>
2025-11-12 12:51:12 +0100

    Connect to database

--- 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
+++ 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
@@ -6,12 +6,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class RealEstateGuiController implements Initializable{
     @FXML public ListView<String> listView;
     @FXML
     private Label welcomeText;
+
+    public Connection conn;
+
+    public void debugger(){int i = 0;}
 
     @FXML
     protected void onHelloButtonClick() {
@@ -20,6 +27,11 @@ public class RealEstateGuiController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ingatlanok", "root", "");
+            debugger();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
