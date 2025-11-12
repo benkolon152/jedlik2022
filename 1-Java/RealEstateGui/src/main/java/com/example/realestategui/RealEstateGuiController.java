7ac27895435f2b895f778ffa3fea90d67c1664a2
Benkolon152 <v.benasz@gmail.com>
2025-11-12 12:43:07 +0100

    Prepare listView

--- 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
+++ 1-Java/RealEstateGui/src/main/java/com/example/realestategui/RealEstateGuiController.java
@@ -2,13 +2,24 @@ package com.example.realestategui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
-public class RealEstateGuiController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RealEstateGuiController implements Initializable{
+    @FXML public ListView<String> listView;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
