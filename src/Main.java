6fc1b4dff2019b60e369625d77c2d86056e357c6
Benkolon152 <v.benasz@gmail.com>
2025-11-12 09:04:40 +0100

    Solve task 6

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,25 @@ public class Main {
     public static void debugger() {int i = 0;}
 
     public static void main(String[] args) throws IOException {
-        System.out.println("RealEstate solution by Benjamin Ver");
-        System.out.println();
+        //System.out.println("RealEstate solution by Benjamin Ver");
+        //System.out.println();
 
         ads = Ad.LoadFromCsv("src/realestates.csv");
+
+        //6.feladat:
+        int sumOfFloor0Areas = 0;
+        int countOfFloor0 = 0;
+        for (Ad ad: ads){
+            if (ad.floors == 0){
+                sumOfFloor0Areas += ad.area;
+                countOfFloor0++;
+            }
+        }
+        double avgOfFloor0Areas = 1.0 * sumOfFloor0Areas / countOfFloor0;
+        System.out.print("1. Földszinti ingatlanok átlagos alapterülete: ");
+        System.out.print(Math.round(100.00 * avgOfFloor0Areas) / 100.00);
+        System.out.println(" m2");
+
         debugger();
     }
 }
