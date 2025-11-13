33bea93aa2e0f6f96688d46b326dde986c9e0f69
Benkolon152 <v.benasz@gmail.com>
2025-11-13 08:36:27 +0100

    get api

--- 3-Backend/react-app/src/Ver_Benjamin_backend/Ver_Benjamin_backend.cjs
+++ 3-Backend/react-app/src/Ver_Benjamin_backend/Ver_Benjamin_backend.cjs
@@ -12,3 +12,22 @@ app.listen(port, err => {
     if (err) console.warn(err)
     else console.log(`Server is running on port ${port}`)
 })
+
+app.get('/api/ingatlan', (req, res) => {
+    const sql = 'SELECT ingatlanok.id, ingatlanok.kategoria, ingatlanok.leirar, ' +
+                    'ingatlanok.hirdetesDatuma, ingatlanok.tehermentes, ' +
+                    'ingatlanok.ar, ingatlanok.kepURL, ' +
+                'FROM ingatlanok ' +
+                'LEFT JOIN kategoriak ON ' +
+                    'ingatlanok.kategoria = kategoriak.id'
+    conn.query(sql, (err, results) => {
+        if (err) {
+            console.warn(err)
+            res.sendStatus(500)
+            return
+        } else {
+            console.log(results)
+            res.sendStatus(200).json(result)
+        }
+    })
+})
